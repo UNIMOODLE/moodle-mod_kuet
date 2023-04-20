e808f195e1fe05322f59c77e6435621f6aa1115d
jmtomas <jmtomas@tresipunt.com>
2023-04-20 14:04:52 +0200

    UNIMOOD-81: first layout for multichoice

--- classes/output/renderer.php
+++ classes/output/renderer.php
@@ -58,8 +58,37 @@ class renderer extends plugin_renderer_base {
      * @throws moodle_exception
      */
     public function render_teacher_view(teacher_view $view): string {
-        $data = $view->export_for_template($this);
-        return $this->render_from_template('mod_jqshow/sessions', $data);
+        /*$data = $view->export_for_template($this);
+        return $this->render_from_template('mod_jqshow/sessions', $data);*/
+        $data = new \stdClass();
+        $data->questionid = 3;
+        $data->question_index_string = 'Pregunta 3 de 10';
+        $data->sessionprogress = 33;
+        $data->multichoice = true;
+        $data->questiontext = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus sit amet turpis id odio fringilla imperdiet. Nullam diam est, efficitur sed condimentum egestas, consectetur eu libero. <img src="https://atodomotor.blob.core.windows.net/contents/article/8062/enriquecruz.jpg">';
+        $data->hastime = true;
+        $data->seconds = 20;
+        $data->managesession = true;
+        $data->numusers = '12/20';
+        $data->answer = [
+            0 => [
+                'answerid' => 1,
+                'answertext' => 'China' // HTML text.
+            ],
+            1 => [
+                'answerid' => 2,
+                'answertext' => 'Alemania'
+            ],
+            2 => [
+                'answerid' => 3,
+                'answertext' => 'Rusia'
+            ],
+            3 => [
+                'answerid' => 4,
+                'answertext' => 'Francia'
+            ]
+        ];
+        return $this->render_from_template('mod_jqshow/questions/encasement', $data);
     }
 
     public function render_teacher_session_view(teacher_session_view $view): string {
