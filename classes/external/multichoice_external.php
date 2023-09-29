22a94339d2909c6ed1a58a76d7d004e321389324
jmtomas <jmtomas@tresipunt.com>
2023-09-29 12:53:43 +0200

    UNIMOOD-165: after merge

--- classes/external/multichoice_external.php
+++ classes/external/multichoice_external.php
@@ -129,7 +129,7 @@ class multichoice_external extends external_api {
                             $cmid, $answer->feedback, 1, $answer->id, $question, 'answerfeedback'
                         ) . '<br>';
                     } else if ((int)$key === (int)$arrayanswer) {
-                        $answertexts[$answer->id] = strip_tags($answer->answer);
+                        $answertexts[$answer->id] = strip_tags($answer->answer); // TODO pass to base64 to avoid json errors.
                     }
                 }
             }
