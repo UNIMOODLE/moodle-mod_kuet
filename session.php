0223778baff0359dbb7be24001f697044d2f5b9c
elena <elena@tresipunt.com>
2023-11-02 15:11:44 +0100

    UNIMOOD-190: not necessary on production

--- session.php
+++ session.php
@@ -53,7 +53,6 @@ $isteacher = has_capability('mod/jqshow:managesessions', $cmcontext);
 $strjqshow = get_string('modulename', 'jqshow');
 $PAGE->set_heading($course->fullname);
 $PAGE->set_title(get_string('session', 'jqshow')) . ' - ' . jqshow_sessions::get_sessionname($jqshow->id, $sid);
-$PAGE->set_cacheable(false); // TODO only for develop.
 
 $activesession = jqshow_sessions::get_active_session_id($jqshow->id);
 if ($activesession !== 0 && $activesession !== $sid) {
