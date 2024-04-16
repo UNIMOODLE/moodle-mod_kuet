b3ff22a6008b97dbb78652defc36935117f1151b
Juan Pablo de Castro <juan.pablo.de.castro@gmail.com>
2024-04-16 08:15:33 +0000

    Github action for publishing plugin

--- version.php
+++ version.php
@@ -34,9 +34,9 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = 2023120100;
+$plugin->version = 2024041600;
 $plugin->requires = 2022112802; // Moodle 4.1.2+.
 $plugin->component = 'mod_kuet';
 $plugin->cron = 0;
 $plugin->maturity = MATURITY_STABLE;
-$plugin->release = 'v0.0.1';
+$plugin->release = 'v0.0.2';
